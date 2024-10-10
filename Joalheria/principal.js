4027865e85fa03de51c417bc4783434934c87edd
jacksongabriel28 <jacksoncorporacao@gmail.com>
2024-10-09 22:53:30 -0300

    Projeto de Joalheria para as matérias de Front-End

---
+++ Joalheria/principal.js
@@ -0,0 +1,21 @@
+function imagempro1(){
+    
+    var produto1= document.getElementById("produto1")
+    produto1.setAttribute("src", "logo.png")
+
+}
+function mudarimagempro1(){
+   
+    var produto1= document.getElementById("produto1")
+    produto1.setAttribute("src","anelproduto1.png")
+
+}
+function imagempro2(){
+    
+    var produto2= document.getElementById("produto2")
+    produto2.setAttribute("src","logo.png")
+}
+function mudarimagempro2(){
+    var produto2= document.getElementById("produto2")
+    produto2.setAttribute("src","colarproduto2.png")
+}
